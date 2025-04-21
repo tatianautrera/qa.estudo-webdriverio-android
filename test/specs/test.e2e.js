6d0fe1ebf03a7310f21fdb28ae88f04534905695
tatiana.utrera <tatiana.utrera@5by5.com.br>
2025-04-20 22:45:56 -0300

    setup inicial

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,10 @@
+import { expect } from '@wdio/globals'
+import LoginPage from '../../y/login.page.js'
+import SecurePage from '../../y/secure.page.js'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await driver.pause(5000)
+    })
+})
+
